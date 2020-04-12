d5e4eeec2132ccfd2e82c01da09b61f362215794
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-12 20:28:04 +0000

    Automatically backed up by Learn

--- final_hash.rb
+++ final_hash.rb
@@ -33,22 +33,3 @@ monopoly =  {
     }
   }
 }
-
-def base_hash
-  monopoly = {
-    :railroads = > {
-    }
-  }
-end 
-
-def monopoly_with_second_tier
-   monopoly[:railroads][:pieces][4]
-   monopoly[:rent_in_dollars][]
-   monopoly[:names][]
-end 
- 
- def monopoly_with_third_tier 
-   monopoly[:railroads][:pieces][4]
-   monopoly[:rent_in_dollars][:one_piece_owned][25]
-   monopoly[:names][]
- end 
