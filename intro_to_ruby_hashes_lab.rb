8782bde4d8af0a2faa4af3f7f5dee50d591842e2
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-12 20:43:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,6 +48,8 @@ def monopoly_with_fourth_tier
       :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        
       }
       }
     }
